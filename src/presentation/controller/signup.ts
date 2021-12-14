70f2fc6ef1fafd87678c0868c1f4794ac10b0b18
ouwargui <guikemon2@gmail.com>
2021-12-13 23:00:11 -0300

    feat: ensure signupcontroller returns 500 if EmailValidator throws

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -4,6 +4,7 @@ import {badRequest} from '../helpers/http-helper';
 import {Controller} from '../protocols/controller';
 import {EmailValidator} from '../protocols/email-validator';
 import {InvalidParamError} from '../errors/invalid-param-error';
+import {ServerError} from '../errors/server-error';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -13,18 +14,25 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: httpRequest): httpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field));
-    }
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field));
+      }
 
-    if (!this.emailValidator.isValid(httpRequest.body.email)) {
-      return badRequest(new InvalidParamError('email'));
-    }
+      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+        return badRequest(new InvalidParamError('email'));
+      }
 
-    return {
-      statusCode: 200,
-      body: {}
-    };
+      return {
+        statusCode: 200,
+        body: {}
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      };
+    }
   }
 }

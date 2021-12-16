d190be3bc4f2f8830cfb0477be317f297c1e195d
ouwargui <guikemon2@gmail.com>
2021-12-16 01:18:38 -0300

    refactor: move middlewares to respective folders

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,7 @@
 import {Express} from 'express';
-import {bodyParser} from '../middlewares/body-parser';
-import {contentType} from '../middlewares/content-type';
-import {cors} from '../middlewares/cors';
+import {bodyParser} from '../middlewares/body-parser/body-parser';
+import {contentType} from '../middlewares/content-type/content-type';
+import {cors} from '../middlewares/cors/cors';
 
 export default (app: Express): void => {
   app.use(bodyParser);

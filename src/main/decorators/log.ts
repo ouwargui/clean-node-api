88ac2e45af32cec5cb016be4919e88e611320731
ouwargui <guikemon2@gmail.com>
2021-12-31 19:14:30 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,14 @@
+import {Controller, httpRequest, httpResponse} from '../../presentation/protocols';
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller;
+
+  constructor (controller: Controller) {
+    this.controller = controller;
+  }
+
+  async handle (httpRequest: httpRequest): Promise<httpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest);
+    return httpResponse;
+  }
+}
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,14 @@ import {SignUpController} from '../../presentation/controller/signup/signup';
 import {EmailValidatorAdapter} from '../../utils/email-validator-adapter';
 import {BcryptAdapter} from '../../infra/criptography/bcrypt-adapter';
 import {AccountMongoRepository} from '../../infra/db/mongodb/account-repository/account';
+import {Controller} from '../../presentation/protocols';
+import {LogControllerDecorator} from '../decorators/log';
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const SALT = 12;
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const accountMongoRepository = new AccountMongoRepository();
   const bcryptAdapter = new BcryptAdapter(SALT);
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
-  return new SignUpController(emailValidatorAdapter, dbAddAccount);
+  return new LogControllerDecorator(new SignUpController(emailValidatorAdapter, dbAddAccount));
 };

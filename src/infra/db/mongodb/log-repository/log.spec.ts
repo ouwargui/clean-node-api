51fd0e350be2869eb75d0c24004fd272102cb983
ouwargui <guikemon2@gmail.com>
2022-01-02 02:53:45 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,27 @@
+import {Collection} from 'mongodb';
+import {MongoHelper} from '../helpers/mongo-helper';
+import {LogMongoRepository} from './log';
+
+let errorsCollection: Collection;
+
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL as string);
+});
+
+afterAll(async () => {
+  await MongoHelper.disconnect();
+});
+
+beforeEach(async () => {
+  errorsCollection = await MongoHelper.getCollection('errors');
+  await errorsCollection.deleteMany({});
+});
+
+describe('Log Mongo Repository', () => {
+  test('should create an error log on success', async () => {
+    const sut = new LogMongoRepository();
+    await sut.logError('any_error');
+    const count = await errorsCollection.countDocuments();
+    expect(count).toBe(1);
+  });
+});

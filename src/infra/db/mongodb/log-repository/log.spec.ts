3b4031816c5ce339e205dd9d85df65161a2888c6
ouwargui <guikemon2@gmail.com>
2022-01-02 02:54:56 -0300

    refactor: move sut creation to a factory method

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -17,9 +17,13 @@ beforeEach(async () => {
   await errorsCollection.deleteMany({});
 });
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository();
+};
+
 describe('Log Mongo Repository', () => {
   test('should create an error log on success', async () => {
-    const sut = new LogMongoRepository();
+    const sut = makeSut();
     await sut.logError('any_error');
     const count = await errorsCollection.countDocuments();
     expect(count).toBe(1);

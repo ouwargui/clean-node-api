e2c30da5726528e39251af6eaf907a90f2e28f52
ouwargui <guikemon2@gmail.com>
2022-01-03 19:46:04 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -1,5 +1,5 @@
 import {InvalidParamError, MissingParamError} from '../../errors';
-import {badRequest, serverError, unauthorized} from '../../helpers/http-helper';
+import {badRequest, ok, serverError, unauthorized} from '../../helpers/http-helper';
 import {
   Controller,
   httpRequest,
@@ -32,7 +32,7 @@ export class LoginController implements Controller {
       const accessToken = await this.authentication.auth(email, password);
       if (!accessToken) return unauthorized();
 
-      return new Promise(resolve => resolve(null as unknown as httpResponse));
+      return ok({accessToken});
     } catch (error: any) {
       return serverError(error);
     }

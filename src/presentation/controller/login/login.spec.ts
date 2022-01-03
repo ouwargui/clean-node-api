921c4d6511a7ddb4321a9dc626a0fc0777651bbb
ouwargui <guikemon2@gmail.com>
2022-01-03 19:40:15 -0300

    refactor: group LoginController dependencies in a single file

--- src/presentation/controller/login/login.spec.ts
+++ src/presentation/controller/login/login.spec.ts
@@ -1,8 +1,7 @@
 import {LoginController} from './login';
 import {badRequest, serverError, unauthorized} from '../../helpers/http-helper';
 import {InvalidParamError, MissingParamError} from '../../errors';
-import {EmailValidator, httpRequest} from '../signup/signup-protocols';
-import {Authentication} from '../../../domain/usecases/authentication';
+import {EmailValidator, httpRequest, Authentication} from './login-protocols';
 
 const makeFakeRequest = (): httpRequest => ({
   body: {
--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -1,8 +1,12 @@
-import {Authentication} from '../../../domain/usecases/authentication';
 import {InvalidParamError, MissingParamError} from '../../errors';
 import {badRequest, serverError, unauthorized} from '../../helpers/http-helper';
-import {Controller, httpRequest, httpResponse} from '../../protocols';
-import {EmailValidator} from '../signup/signup-protocols';
+import {
+  Controller,
+  httpRequest,
+  httpResponse,
+  EmailValidator,
+  Authentication
+} from './login-protocols';
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator;
